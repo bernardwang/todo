ac529663a10892bda31a1866bb1ec508c4248c59
bw-vbnm <bwang19@illinois.edu>
2015-01-01 15:15:46 -0500

    fixed server.js, removed templating

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-// setup
-///////////////////////////////////////////////////////////
+///// setup /////
 var express = require('express'); 
 var app = express();  
 var mongoose = require('mongoose');
@@ -7,9 +6,7 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-// config 
-///////////////////////////////////////////////////////////
-
+///// config /////
 var port = process.env.PORT || 3000; 
 
 // connect to mongodb
@@ -30,35 +27,10 @@ app.use(methodOverride());
 
 var Todo = require('./app/models/todo.js');
 
-// routes
-///////////////////////////////////////////////////////////
+///// routes /////
 require('./app/routes/api.js')(app);
-//require('./app/routes/index.js')(app);
+require('./app/routes/index.js');
 
-
-// start
-///////////////////////////////////////////////////////////
-
+///// start /////
 app.listen(port);
 console.log("App listening on port "+port);
-//exports = module.exports = app; 
-
-/*/ ROUTES FOR OUR API
-
-var router = express.Router();            
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
-});
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);*/
